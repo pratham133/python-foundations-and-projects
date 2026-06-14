789ca1768b1bfdc7c52303a17a1376a56b238604
Pratham Pasi <prathampasi124@gmail.com>
2026-06-14 16:01:49 +0530

    Day 4: Added operators and calculator programs

---
+++ operators.py
@@ -0,0 +1,77 @@
+# Program 1 (Math Operators)
+
+# Get numbers
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+# Math Operations
+addition = num1 + num2
+subtraction = num1 - num2
+multiplication = num1 * num2
+division = num1 / num2
+
+# Show result
+print("Addition:", addition)
+print("Subtraction:", subtraction)
+print("Multiplication:", multiplication)
+print("Divison:", division)
+
+
+# Program 2 (More Operators)
+ # // → Floor Division
+ # %  → Modulus (Remainder)
+ # ** → Power
+
+# Get numbers
+num1 = int(input("Enter First number: "))
+num2 = int(input("Enter Second number: "))
+
+# More Operations
+floor_division = num1 // num2
+remainder = num1 % num2
+power = num1 ** num2
+
+# Show result
+print("Floor Division:",floor_division)
+print("Remainder:", remainder)
+print("Power:", power)
+
+# Program 3 (Square and Cube Calculator)
+
+# Get number input
+number = int(input("Enter the number: "))
+
+# calculate square and cube
+square = number ** 2
+cube = number ** 3
+
+# Show result
+print("Square:", square)
+print("Cube:", cube)
+
+# Program 4 (Simple Calculator)
+
+# Get numbers
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+# Show result
+print("Addition:", num1 + num2)
+print("Subtraction:", num1 - num2)
+print("Multiplication:", num1 * num2)
+print("Division:", num1 / num2)
+print("Floor Division:", num1 // num2)
+print("Remainder:", num1 % num2)
+print("Power:", num1 ** num2)
+
+# Program 5 (Area of Rectangle)
+
+# Get length and width
+length = int(input("Enter Length: "))
+width = int(input("Enter Width: "))
+
+# Calculate area
+area = length * width
+
+# Show result
+print("Area of rectangle:", area)
